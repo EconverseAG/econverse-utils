393d901cc4b948c80978c34d37c609c38bf4e6b8
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-30 09:04:42 -0300

    feat: formatCep to main export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import formatMoney from './utils/formatMoney';
 import unformatMoney from './utils/unformatMoney';
 
 import textEllipsis from './utils/textEllipsis';
+import formatCep from './utils/formatCep';
 
-export { formatMoney, unformatMoney, textEllipsis };
+export { formatMoney, unformatMoney, textEllipsis, formatCep };
