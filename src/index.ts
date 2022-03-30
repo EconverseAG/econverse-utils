370879ddf8627c2c0541290b9128635d9ae788a5
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-29 21:11:10 -0300

    chore: eslint ignore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('>>> Hello world!');
+function helloWorld() {
+  console.log('>>> hello world');
+}
+
+function byeWorld() {
+  console.log('>>> hello world');
+}
+
+export { helloWorld, byeWorld };
