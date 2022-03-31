a2fe5fdd49b24ec5bfe0ab1511b9e5ddab38821e
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-03-31 17:18:17 -0300

    fix: broken imports

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import textEllipsis from './utils/textEllipsis';
 import formatCep from './utils/formatCep';
 
 import isCnpj from './utils/isCnpj';
+import isCpf from './utils/isCpf';
 
-export { formatMoney, unformatMoney, textEllipsis, formatCep, isCnpj, isCnpj };
+export { formatMoney, unformatMoney, textEllipsis, formatCep, isCnpj, isCpf };
