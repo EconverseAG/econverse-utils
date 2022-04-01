035ca30545d2bb95763b2c5768f0b25e0e26e298
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-01 15:57:43 -0300

    chore: add capitalizeFirstLetter to exports

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,17 @@ import unformatMoney from './utils/unformatMoney';
 
 import textEllipsis from './utils/textEllipsis';
 import formatCep from './utils/formatCep';
+import capitalizeFirstLetter from './utils/capitalizeFirstLetter';
 
 import isCnpj from './utils/isCnpj';
 import isCpf from './utils/isCpf';
 
-export { formatMoney, unformatMoney, textEllipsis, formatCep, isCnpj, isCpf };
+export {
+  formatMoney,
+  unformatMoney,
+  textEllipsis,
+  formatCep,
+  isCnpj,
+  isCpf,
+  capitalizeFirstLetter,
+};
