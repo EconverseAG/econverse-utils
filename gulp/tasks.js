9f27f2e64d3c78628cdf773df71efef75d9b31d5
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-04-05 10:18:04 -0300

    chore(gulp): builds are separated into lib and vtex in concatted files

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import gulp from 'gulp';
+import concat from 'gulp-concat';
 import * as Paths from './paths';
 import * as Pipes from './pipes';
 import ts from 'gulp-typescript';
 
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json', {
+  declaration: true,
+});
 
 export const clear = callback => {
   if (fs.existsSync(Paths.Folders.lib))
@@ -13,7 +16,23 @@ export const clear = callback => {
 };
 
 export const scripts = callback => {
-  const source = tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('lib'));
+  const source = gulp
+    .src(['./src/index.ts', './src/utils/**/*.ts', '!src/**/*.test.ts'])
+    .pipe(concat('index.ts'))
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('lib'));
+
+  return Pipes.Scripts(source, () => {
+    if (typeof callback === 'function') callback();
+  });
+};
+
+export const vtexUtils = callback => {
+  const source = gulp
+    .src(['./src/vtex/index.ts', './src/vtex/**/*.ts', '!src/**/*.test.ts'])
+    .pipe(concat('index.ts'))
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('vtex'));
 
   return Pipes.Scripts(source, () => {
     if (typeof callback === 'function') callback();
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,6 @@
 import { series } from 'gulp';
-import { clear, scripts } from './gulp/tasks';
+import { clear, scripts, vtexUtils } from './gulp/tasks';
 
-export const build = series(clear, scripts);
+export const build = series(clear, scripts, vtexUtils);
 
 export default build;
